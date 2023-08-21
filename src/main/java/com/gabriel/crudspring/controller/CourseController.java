963783c45e372a4146b7cdb2ac64646644f78c5c
Gabriel de Aguiar <gabrieldeaguiarbrito737@gmail.com>
2023-08-21 11:56:58 -0300

    create course - http post

--- src/main/java/com/gabriel/crudspring/controller/CourseController.java
+++ src/main/java/com/gabriel/crudspring/controller/CourseController.java
@@ -4,10 +4,9 @@ import com.gabriel.crudspring.model.Course;
 import com.gabriel.crudspring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,5 +20,12 @@ public class CourseController {
     @GetMapping
     public @ResponseBody List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+        return courseRepository.save(course);
+         //return ResponseEntity.status(HttpStatus.CREATED.body(courseRepository.save(course));
     }
 }
