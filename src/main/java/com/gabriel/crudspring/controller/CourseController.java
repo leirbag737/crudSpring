517e5270f58c2e48cfd50f1e9a59ecbd8fa5dc4e
Gabriel de Aguiar <gabrieldeaguiarbrito737@gmail.com>
2023-08-22 09:50:58 -0300

    find by id

--- src/main/java/com/gabriel/crudspring/controller/CourseController.java
+++ src/main/java/com/gabriel/crudspring/controller/CourseController.java
@@ -22,10 +22,19 @@ public class CourseController {
         return courseRepository.findAll();
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id){
+        return courseRepository.findById(id)
+                .map(record -> ResponseEntity.ok().body(record))
+                .orElse(ResponseEntity.notFound().build()); // se o optional não vir nulo ele retorna o course, caso nulo retorna 404
+    }
+
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public Course create(@RequestBody Course course){
         return courseRepository.save(course);
          //return ResponseEntity.status(HttpStatus.CREATED.body(courseRepository.save(course));
     }
+
+
 }
